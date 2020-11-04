a89156238ccaa0dccebd1831a5861f6617129a34
BeneZ <bezraat@gmail.com>
2020-11-04 15:47:36 +0100

    second commit

--- src/main/java/week02/Product.java
+++ src/main/java/week02/Product.java
@@ -22,14 +22,20 @@ public class Product {
 
         return name.equals(anotherProduct.name);
     }
+    public boolean areTheyEqual2(Product anotherProduct) {
+
+        return name.equals(anotherProduct.name) && Math.abs(code.length()-anotherProduct.code.length())<=1;
+    }
+
 
     public static void main(String[] args) {
         Product product1=new Product("alma","1234");
         Product product2=new Product("körte","3456");
-        Product product3=new Product("alma","12345");
+        Product product3=new Product("alma","123456");
 
         System.out.println(product1.areTheyEqual(product2));
         System.out.println(product1.areTheyEqual(product3));
+        System.out.println(product1.areTheyEqual2(product3));
 
 
     }
