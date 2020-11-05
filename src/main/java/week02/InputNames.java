2f6de12274e3b86be0e9889cbb3a9d5274449402
BeneZ <bezraat@gmail.com>
2020-11-05 15:40:11 +0100

    commit first

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,18 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+    public static void main(String[] args) {
+        String[] array= new String[5];
+        Scanner scanner=new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Adj meg egy nevet:");
+            array[i]= scanner.nextLine();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i+1)+". elem: "+ array[i]);
+
+        }
+    }
+}
