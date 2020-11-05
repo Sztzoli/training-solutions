e6c2f37a544f0e2a5e617e8a6541ad5d95fe555a
BeneZ <bezraat@gmail.com>
2020-11-05 17:12:21 +0100

    commit

--- src/main/java/math/Game.java
+++ src/main/java/math/Game.java
@@ -9,37 +9,31 @@ public class Game {
         Warrior ork = new Warrior("Thrall", new Point(20, 0));
 
 
-        int round = 1;
 
-        for (int i = 0; i < 1; i++) {
-            nightElf.move(ork);
-            ork.move(nightElf);
-            if (nightElf.getPosition().getX() == ork.getPosition().getX() && nightElf.getPosition().getY() == ork.getPosition().getY()) {
-                i++;
+
+        for (int round = 1; nightElf.isAlive() && ork.isAlive()  ; round++) {
+            if (nightElf.getPosition().distance(ork.getPosition())!=0){
+                nightElf.move(ork);
+                ork.move(nightElf);
+                round++;
+            } else {
+                nightElf.attack(ork);
+                if (ork.isAlive()){
+                    ork.attack(nightElf);
+                }
             }
             System.out.println(round+". kör:");
             round++;
             System.out.println(nightElf.toString());
             System.out.println(ork.toString());
-            i--;
-        }
 
-        for (int i = 0; i < 1; i++) {
-            nightElf.attack(ork);
-            ork.attack(nightElf);
-            if (ork.isAlive() && nightElf.isAlive()){
-                System.out.println(round+". kör:");
-                round++;
-                System.out.println(nightElf.toString());
-                System.out.println(ork.toString());
-                i--;
-            }
 
 
         }
-        System.out.println(round+". kör:");
-        System.out.println(nightElf.toString());
-        System.out.println(ork.toString());
+
+
+
+
 
         System.out.println(nightElf.isAlive() ? "Winner: "+nightElf.toString() : "Winner: " +ork.toString());
 
