d15769442a1f7a401c4c6cbb42c6141ff380a5b7
BeneZ <bezraat@gmail.com>
2020-11-12 09:04:58 +0100

    commit after test

--- src/main/java/stringconcat/stringconcat/Name.java
+++ src/main/java/stringconcat/stringconcat/Name.java
@@ -8,53 +8,56 @@ public class Name {
 
     public Name(String familyName, String middleName, String givenName, Title title) {
         this(familyName, middleName, givenName);
-        this.title=title;
+        this.title = title;
     }
 
     public Name(String familyName, String middleName, String givenName) {
-        if (!isEmpty(familyName) || !isEmpty(givenName)){
+        if (!isEmpty(familyName) || !isEmpty(givenName)) {
             throw new IllegalArgumentException("üres a név");
         }
-        this.familyName=familyName;
-        this.middleName=middleName;
-        this.givenName=givenName;
+        this.familyName = familyName;
+        this.middleName = middleName;
+        this.givenName = givenName;
     }
 
     public String concatNameWesternStyle() {
-        String s="";
+        String s = "";
         if (title != null) {
             s = s.concat(title.getTitle());
         }
-        s=s.concat(givenName);
-        s=s.concat(" ");
-        if (!middleName.isEmpty()){
-            s=s.concat(middleName);
-            s=s.concat(" ");
+        s = s.concat(givenName);
+        s = s.concat(" ");
+
+        if (isEmpty(middleName)) {
+            s = s.concat(middleName);
+            s = s.concat(" ");
         }
-        s=s.concat(familyName);
+        s = s.concat(familyName);
+
         return s;
     }
 
     public String concatNameHungarianStyle() {
-       String s="";
-       if (title != null) {
-           s+=title.getTitle();
-       }
-        s+=familyName;
-       s+=" ";
-       if (!middleName.isEmpty()) {
-           s+=givenName;
-           s+=" ";
-       }
+        String s = "";
+        if (title != null) {
+            s += title.getTitle();
+        }
+        s += familyName;
+        s += " ";
+        s += givenName;
+        if (isEmpty(middleName)) {
+            s += " ";
+            s += middleName;
+        }
 
-        s+=middleName;
+
         return s;
     }
 
     private boolean isEmpty(String name) {
         boolean a = false;
-        if (name!=null && !name.isEmpty()){
-            a=true;
+        if (name != null && !"".equals(name)) {
+            a = true;
         }
         return a;
     }
--- src/test/java/stringconcat/stringconcat/NameTest.java
+++ src/test/java/stringconcat/stringconcat/NameTest.java
@@ -7,14 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NameTest {
 
     @Test
-    public void testConcatNameWesternStyle() {
+    public void ConcatStyleTest() {
         Name name= new Name("Kovács","Ferenc","János",Title.DR);
-        System.out.println(name.concatNameWesternStyle());
+        Name nameWithoutTitle=new Name("Kovács","Ferenc","János");
+        Name middleNameIsNull=new Name("Kovács",null,"János",Title.DR);
+        Name middleNameIsEmpty = new Name("Kovács","","János",Title.DR);
+        //Western
+        assertEquals("Dr.János Ferenc Kovács",name.concatNameWesternStyle());
+        assertEquals("János Ferenc Kovács",nameWithoutTitle.concatNameWesternStyle());
+        assertEquals("Dr.János Kovács",middleNameIsNull.concatNameWesternStyle());
+        assertEquals("Dr.János Kovács",middleNameIsNull.concatNameWesternStyle());
+
+        //Hungarian
+        assertEquals("Dr.Kovács János Ferenc",name.concatNameHungarianStyle());
+        assertEquals("Dr.Kovács János",middleNameIsNull.concatNameHungarianStyle());
+        assertEquals("Dr.Kovács János",middleNameIsNull.concatNameHungarianStyle());
     }
 
     @Test
-    public void concatNameHungarianStyle() {
-        Name name= new Name("Kovács","Ferenc","János",Title.DR);
-        System.out.println(name.concatNameHungarianStyle());
+    public void illegalArgumentExceptionTest() {
+        // Family name
+            //null
+        Exception exFamilyName = assertThrows(IllegalArgumentException.class , ()->new Name(null,"Ferenc","János",Title.DR));
+        assertEquals("üres a név",exFamilyName.getMessage());
+            //empty
+        Exception exFamilyNameEmpty = assertThrows(IllegalArgumentException.class , ()->new Name("","Ferenc","János",Title.DR));
+        assertEquals("üres a név",exFamilyNameEmpty.getMessage());
+        //Given Name
+            //null
+        Exception exGivenName = assertThrows(IllegalArgumentException.class , ()->new Name("János","Ferenc",null,Title.DR));
+        assertEquals("üres a név",exGivenName.getMessage());
+            //empty
+        Exception exGivenNameEmpty = assertThrows(IllegalArgumentException.class , ()->new Name("János","Ferenc","",Title.DR));
+        assertEquals("üres a név",exGivenNameEmpty.getMessage());
     }
 }
