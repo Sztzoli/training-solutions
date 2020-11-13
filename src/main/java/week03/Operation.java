1e0a398715d6d1f5b1874df342d9d04fb8de9a8d
BeneZ <bezraat@gmail.com>
2020-11-13 15:52:04 +0100

    commit first

---
+++ src/main/java/week03/Operation.java
@@ -0,0 +1,16 @@
+package week03;
+
+public class Operation {
+    private int left;
+    private int right;
+
+    public Operation(String str) {
+        left=Integer.parseInt(str.substring(0,str.indexOf("+")));
+        right=Integer.parseInt(str.substring(str.indexOf("+")+1));
+
+    }
+    public int getResult(){
+        return left+right;
+    }
+
+}
---
+++ src/test/java/week03/OperationTest.java
@@ -0,0 +1,18 @@
+package week03;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OperationTest {
+
+    @Test
+    public void TestCreate() {
+        Operation operation = new Operation("123+45");
+
+        assertEquals(123+45,operation.getResult());
+    }
+
+
+}
