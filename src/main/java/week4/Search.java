89491cda7d4ed5fc6311b31527ea5cbe4afb4475
BeneZ <bezraat@gmail.com>
2020-11-20 15:53:01 +0100

    commit

---
+++ src/main/java/week4/Search.java
@@ -0,0 +1,18 @@
+package week4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search {
+
+    public List<Integer> search(String word, char c){
+        List<Integer> result= new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            if (c==word.charAt(i)){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+}
