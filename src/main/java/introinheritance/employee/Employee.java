e71b56ef5bfc23a4c34788a064a473e7b7876ebc
BeneZ <bezraat@gmail.com>
2020-11-26 21:01:42 +0100

    commit

---
+++ src/main/java/introinheritance/employee/Employee.java
@@ -0,0 +1,24 @@
+package introinheritance.employee;
+
+public class Employee extends Person {
+    private double salary;
+
+    public Employee(String name, String address, double salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(int percent) {
+        double raise = salary * percent / 100d;
+        salary += raise;
+    }
+
+
+
+}
---
+++ src/main/java/introinheritance/employee/Person.java
@@ -0,0 +1,39 @@
+package introinheritance.employee;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        if (isEmpty(name)){
+            throw new IllegalArgumentException("name is wrong");
+        }
+        this.name = name;
+        if (isEmpty(address)){
+            throw new IllegalArgumentException( "address is wrong");
+        }
+        this.address = address;
+    }
+
+    public void migrate(String newAddress){
+        if (isEmpty(newAddress)){
+            throw new IllegalArgumentException("newAddress is wrong");
+        }
+        address=newAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public boolean isEmpty(String str){
+        if (str==null || str.isBlank()) {
+            return false;
+        }
+        return true;
+    }
+}
