0841ca50bbe3c0d40b624bb7748b85ca575a9340
BeneZ <bezraat@gmail.com>
2020-11-30 18:01:50 +0100

    commit

--- src/test/java/week06d01/SeparatedSumTest.java
+++ src/test/java/week06d01/SeparatedSumTest.java
@@ -11,8 +11,24 @@ class SeparatedSumTest {
         SeparatedSum separatedSum = new SeparatedSum();
         String s="4,5;-1.4;5";
         Result result=separatedSum.sum(s);
-        System.out.println(result.getPositiveSum());
-        System.out.println(result.getNegativeSum());
+        assertEquals(9.5, result.getPositiveSum(),0.005);
+        assertEquals(-1.4,result.getNegativeSum(),0.005);
+
     }
+
+    @Test
+    void ExceptionNull(){
+
+        Exception ex= assertThrows(IllegalArgumentException.class, ()->new SeparatedSum().sum(null));
+        assertEquals("String can't be null or blank",ex.getMessage());
+    }
+
+    @Test
+    void ExceptionEmpty(){
+
+        Exception ex= assertThrows(IllegalArgumentException.class, ()->new SeparatedSum().sum("    "));
+        assertEquals("String can't be null or blank",ex.getMessage());
+    }
+
 
 }
