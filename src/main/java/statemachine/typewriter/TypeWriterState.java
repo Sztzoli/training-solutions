2b1462aa9b7b0d71804009b2d52ec5cd0fdfec3f
BeneZ <bezraat@gmail.com>
2020-12-01 16:42:02 +0100

    commit statemachine.writer

---
+++ src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,18 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+    UPPERCASE{
+        @Override
+        TypeWriterState next() {
+            return LOWERCASE;
+        }
+    },
+    LOWERCASE{
+        @Override
+        TypeWriterState next() {
+            return UPPERCASE;
+        }
+    };
+
+    abstract TypeWriterState next();
+}
