146d745032f7412f157420423021a03eb8669dc3
BeneZ <bezraat@gmail.com>
2020-12-05 19:09:32 +0100

    commit exceptionclass Bank2

---
+++ src/main/java/exceptionclass/bank2/Bank.java
@@ -0,0 +1,34 @@
+package exceptionclass.bank2;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Account> accounts = new ArrayList<>();
+
+    public Bank(List<Account> accounts) {
+        if (accounts==null){
+            throw new IllegalArgumentException();
+        }
+        this.accounts = accounts;
+    }
+
+    public void payment(String accountNumber, double amount) {
+        searchByAccountNumber(accountNumber).subtract(amount);
+    }
+
+    public void deposit(String accountNumber, double amount) {
+        searchByAccountNumber(accountNumber).deposit(amount);
+    }
+
+
+    private Account searchByAccountNumber(String accountNumber) {
+        for (Account account : accounts) {
+            if (accountNumber.equals(account.getAccountNumber())) {
+                return account;
+            }
+        }
+        throw new InvalidAccountNumberBankOperationException();
+    }
+}
