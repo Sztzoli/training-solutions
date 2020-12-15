7381801ffd61948ef1d096c2789125d9c56e48bb
BeneZ <bezraat@gmail.com>
2020-12-15 10:03:51 +0100

    week08d02 commit

--- src/main/java/week08d02/CountryStatistics.java
+++ src/main/java/week08d02/CountryStatistics.java
@@ -19,20 +19,22 @@ public class CountryStatistics {
                         CountryStatistics.class.getResourceAsStream("/countries.txt")))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(SEPARATOR);
-                String country = parts[0];
-                int populations = Integer.parseInt(parts[1]);
-                int numberOfColorInFlag = Integer.parseInt(parts[2]);
-                int borderCountries = Integer.parseInt(parts[3]);
-                countries.add(new Country(country, populations, numberOfColorInFlag, borderCountries));
-
+                addCounty(line);
             }
-
-
         } catch (IOException ioe) {
             throw new IllegalStateException("can not read file, ioe");
         }
     }
+
+    private void addCounty (String line){
+        String[] parts = line.split(SEPARATOR);
+        String country = parts[0];
+        int populations = Integer.parseInt(parts[1]);
+        int numberOfColorInFlag = Integer.parseInt(parts[2]);
+        int borderCountries = Integer.parseInt(parts[3]);
+        countries.add(new Country(country, populations, numberOfColorInFlag, borderCountries));
+    }
+
 
     public Country maxPopulation() {
         Country maxCountry = countries.get(0);
