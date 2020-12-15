9c8d8b19e05e690e46e1ebc64d130b3f0f6429ba
BeneZ <bezraat@gmail.com>
2020-12-15 18:50:31 +0100

    iozip names

---
+++ src/main/java/iozip/names/EmployeeFileManager.java
@@ -0,0 +1,24 @@
+package iozip.names;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class EmployeeFileManager {
+
+    public void saveEmployees(Path path, List<String> employees){
+        try (ZipOutputStream outputStream = new ZipOutputStream(new BufferedOutputStream(Files.newOutputStream(path)))) {
+            outputStream.putNextEntry(new ZipEntry("names.dat"));
+            for (String names : employees){
+                outputStream.write(names.getBytes(StandardCharsets.UTF_8));
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("can not write file", ioe);
+        }
+    }
+}
