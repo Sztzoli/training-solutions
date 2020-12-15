190898c2d1f5bdd9425388286a095058c0b358bc
BeneZ <bezraat@gmail.com>
2020-12-15 20:19:17 +0100

    ioconvert product

---
+++ src/main/java/ioconvert/products/ProductWriter.java
@@ -0,0 +1,21 @@
+package ioconvert.products;
+
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+public class ProductWriter {
+
+    public void saveProduct(OutputStream outputStream, List<Product> products) {
+        try (PrintStream printStream = new PrintStream(outputStream)) {
+            for (Product product : products) {
+                printStream.print(product.getName());
+                printStream.print(";");
+                printStream.print(product.getPrice());
+                printStream.println();
+            }
+
+        }
+    }
+}
